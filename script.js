8857d96a43f9f44cf5897a299d040bdcebdae61c
alexdonovan98 <alex98@live.unc.edu>
2019-08-08 21:57:14 -0400

    Alex Updates

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".inner").append("<p>Test</p>");
+    document.getElementById("demo").innerHTML = "My First JavaScript";
+});
+
+document.getElementById("demo").innerHTML = "My First JavaScript";
